34dac564631e3fb116fd7d60f2ff75a04753f71e
Elena Willen <lnamaso@hotmail.com>
2016-05-07 17:08:11 +0100

    Initial Commit

---
+++ Forecaster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Forecaster
+//
+//  Created by Elena Maso Willen on 07/05/2016.
+//  Copyright © 2016 Training. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
